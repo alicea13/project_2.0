897d6f478efbe16224e0ea6bbf6160ef460d73ac
alisa <alica.aisina@yandex.ru>
2020-02-14 02:05:22 +0300

    Фон стартового окна(классы Board, Bubble) + изменения в классах StartWindow, InputText

--- project.py
+++ project.py
@@ -4,8 +4,13 @@ import sqlite3
 import copy
 
 
+
+all_sprites = pygame.sprite.Group()
+
+
 class StartWindow:
     def __init__(self):
+        global music_on
         super().__init__()
         pygame.init()
 
@@ -16,13 +21,30 @@ class StartWindow:
         screen = pygame.display.set_mode(size)
 
         titl = pygame.font.SysFont('gadugi', 36)
-        self.text1 = titl.render("One Little Warm", 1, pygame.Color("blue"))
+        self.text1 = titl.render("One Little Warm", 1, (255, 20, 147))
 
         ask_input = pygame.font.SysFont('arial', 25)
-        self.text2 = ask_input.render("Введите логин:", 1, pygame.Color("lightblue"))
+        self.text2 = ask_input.render("Введите логин:", 1, (0, 49, 83))
 
         ex = False
-        login = InputText(width // 2 - 70, height // 5 * 3 - 32, 140, 32, ex, "lightblue")
+        login = InputText(width // 2 - 70, height // 5 * 3 - 32, 140, 32, ex, (255, 20, 147))
+
+        self.h_border = pygame.sprite.Group()
+        self.v_border = pygame.sprite.Group()
+
+        self.font = pygame.transform.scale(self.load_image('font_4.jpg'), (width, height))
+        self.music = pygame.transform.scale(self.load_image('music_2.png', -1), (60, 60))
+
+
+        clock = pygame.time.Clock()
+
+        Border(5, 5, width - 5, 5, self)
+        Border(5, height - 5, width - 5, height - 5, self)
+        Border(5, 5, 5, height - 5, self)
+        Border(width - 5, 5, width - 5, height - 5, self)
+
+        for i in range(17):
+            Bubble(5, 100, 100, self)
 
         run_st = True
 
@@ -33,26 +55,90 @@ class StartWindow:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         run_st = False
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if 5 <= event.pos[0] <= 65 and 5 <= event.pos[1] <= 65 and music_on:
+                            pygame.mixer.music.pause()
+                            music_on = False
+                            continue
+                        if 5 <= event.pos[0] <= 65 and 5 <= event.pos[1] <= 65 and not music_on:
+                            pygame.mixer.music.unpause()
+                            music_on = True
                     login.events(event)
 
-            screen.fill((30, 30, 30))
+            screen.blit(self.font, (0, 0))
+            screen.blit(self.music, (5, 5))
+            all_sprites.update()
+            all_sprites.draw(screen)  # говорим прорисовывать sprites
             login.draw(screen)
             screen.blit(self.text1, (180, 60))
             screen.blit(self.text2, (220, 170))
+            clock.tick(40)
+
             pygame.display.flip()
+        pygame.quit()
+
+    def load_image(self, name, color_key=None):
+        fullname = os.path.join("data", name)
+        image = pygame.image.load(fullname).convert()
+        if color_key is not None:
+            if color_key == -1:
+                color_key = image.get_at((0, 0))
+            image.set_colorkey(color_key)
+        else:
+            image = image.convert_alpha()
+        return image
 
     def get_click(self, pos):
         print("here")
+
+
+class Bubble(pygame.sprite.Sprite, StartWindow):
+    def __init__(self, radius, x, y, par):
+        super().__init__(all_sprites)
+
+        self.par = par
+
+        list = ["data/bubble_font_1.png", "data/bubble_font_2.png",
+                "data/bubble_font_3.png",
+                "data/bubble_font_4.png"]
+
+        self.image = self.image = pygame.image.load(random.choice(list))
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
+
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+        if pygame.sprite.spritecollideany(self, self.par.h_border):
+            self.vy = -self.vy
+        if pygame.sprite.spritecollideany(self, self.par.v_border):
+            self.vx = -self.vx
+
+
+class Border(pygame.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2, par):
+        super().__init__(all_sprites)
+
+        self.par = par
+        if x1 == x2:
+            self.add(self.par.v_border)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:
+            self.add(self.par.h_border)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
 
 
 class InputText:
     def __init__(self, x, y, width, height, ex, color, text=''):
         self.rect = pygame.Rect(x, y, width, height)
         self.login_t = text
-        self.text_surf = pygame.font.Font(None, 32).render(text, True, pygame.Color('lightblue'))
+        self.color = color
+        self.text_surf = pygame.font.Font(None, 32).render(text, True, (255, 20, 147))
         self.run_text = False
         self.exit = ex
-        self.color = color
 
         self.sound = pygame.mixer.Sound("music/click_sound_cut2.wav")
 
@@ -62,11 +148,12 @@ class InputText:
     def events(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             self.sound.play()
-            self.color = "blue"
             if self.rect.collidepoint(event.pos):
                 self.run_text = True
+                self.color = (0, 49, 73)
             else:
                 self.run_text = False
+                self.color = (255, 20, 147)
 
         if event.type == pygame.KEYDOWN and self.run_text:
             if event.key == pygame.K_RETURN:
@@ -76,7 +163,7 @@ class InputText:
                 self.login_t = self.login_t[:-1]
             else:
                 self.login_t += event.unicode
-            self.text_surf = pygame.font.Font(None, 32).render(self.login_t, True, pygame.Color('lightblue'))
+            self.text_surf = pygame.font.Font(None, 32).render(self.login_t, True, (255, 20, 147))
 
     def open(self, login_t):
         self.id_login = self.cur.execute("""SELECT id FROM logins
@@ -99,7 +186,7 @@ class InputText:
     def draw(self, screen):
         screen.blit(self.text_surf, (self.rect.x + 5, self.rect.y + 5))
 
-        pygame.draw.rect(screen, pygame.Color(self.color), self.rect, 4)
+        pygame.draw.rect(screen, self.color, self.rect, 4)
 
 
 class HaveLogin:
@@ -435,7 +522,7 @@ class Character:
         emoji_7 = AnimatedSprite(self.load_image("emoji_7.png", -1), 3, 3, 40,
                                  410, all_sprites)
 
-        emoji_5 = AnimatedSprite(self.load_image("emoji_5.png", -1), 3, 3, 220,
+        emoji_5 = AnimatedSprite(self.load_image("chicken_ch.png", -1), 3, 3, 220,
                                  110, all_sprites)
 
         emoji_6 = AnimatedSprite(self.load_image("emoji_6.png", -1), 3, 3, 220,
@@ -742,6 +829,9 @@ class Snake(Board):
         self.board = copy.deepcopy(temp)
 
 
+music_on = True
+
+
 def music():
     pygame.init()
     music_sp = ["music/melody_1.mp3", "music/melody_2.mp3",
@@ -750,7 +840,7 @@ def music():
     pygame.mixer.music.play()
 
 
-music()
+music_play = music()
 start = StartWindow()
 
 
